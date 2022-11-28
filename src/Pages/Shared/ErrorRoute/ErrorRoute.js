9cf9fe4f351bf8a4ac641e4ab55b0db61a31ae86
mosraful21 <mosraful21@gmail.com>
2022-11-28 19:42:03 +0600

    errorroute

--- src/Pages/Shared/ErrorRoute/ErrorRoute.js
+++ src/Pages/Shared/ErrorRoute/ErrorRoute.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import img from '../../../Assets/404.png';
 
-
+const ErrorRoute = () => {
+    return (
+        <div>
+            <img className='w-1/2 h-1/2 m-auto' src={img} alt="" />
+            <div className='flex justify-center mt-12'>
+                <Link to='/'> <button className='btn btn-wide'> Home </button></Link>
+            </div>
+        </div>
+    );
+};
 
 export default ErrorRoute;
